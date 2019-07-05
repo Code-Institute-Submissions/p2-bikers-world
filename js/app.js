0db0893cb1d8f94e2150ee8658af4022f71c5dca
Ubuntu <ubuntu@ip-172-31-93-218.ec2.internal>
2019-07-05 08:14:12 +0000

    Arranged the code structure of index.html and js/app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,25 @@
 var map;
 var endpoint = "https://api.data.gov.sg/v1/transport/taxi-availability"
 
-$(function(){
-    $("#get-taxi-button").click(function(){
-      // async - how do we wait for the data to finish
-      // loading before executing the next task?
-      getDataFromEndpointAsync(function(data){
-          
-          // for each taxi inside the data
-          for (let taxi of data)
-          {
-            let taxiPos = {
-                lat: taxi[1],
-                lng: taxi[0]
-              };
-      
-              new google.maps.Marker({
-                  position: taxiPos,
-                  map: map
-              })
-          }
-      });
+$(function() {
+    $("#get-taxi-button").click(function() {
+        // async - how do we wait for the data to finish
+        // loading before executing the next task?
+        getDataFromEndpointAsync(function(data) {
+            
+            // for each taxi inside the data
+            for (let taxi of data) {
+                let taxiPos = {
+                    lat: taxi[1],
+                    lng: taxi[0]
+                };
+
+                new google.maps.Marker({
+                    position: taxiPos,
+                    map: map
+                })
+            }
+        });
     })
 });
 
