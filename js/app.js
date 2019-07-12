21852f85569368ea7a26d1228d8f9d71ab074587
Ubuntu <ubuntu@ip-172-31-93-218.ec2.internal>
2019-07-12 09:09:42 +0000

    Changes made... 1. Remove unncessary sections from the template. 2. Able to retrieve CityBikes API's data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
-var map;
-var endpoint = "https://api.data.gov.sg/v1/transport/taxi-availability"
+// Variables declarations
 
+// --- Store map instance
+var map;
+// --- Store API link
+// CityBikes API 
+var endpoint = "https://api.citybik.es/v2/networks";
+// var endpoint = "http://api.citybik.es/v2/networks";
+
+
+function initMap() {
+    
+}
+
+// data source
+function getDataFromEndpointAsync(callback) {
+    axios.get(endpoint)
+        .then(function(response) {
+            let feedback = response.data.networks[0].location;
+            console.log(feedback);
+            
+            // let results = response.data.features[0].geometry.coordinates;
+            // // trigger the callback
+            // callback(results);
+            // console.log(response)
+        });
+}
+
+// Retrieve data from CityBike API
+getDataFromEndpointAsync();
+
+/*
 $(function() {
     $("#get-taxi-button").click(function() {
         // async - how do we wait for the data to finish
@@ -24,6 +53,7 @@ $(function() {
 });
 
 function initMap() {
+    
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 1.35, lng: 103.81 },
         zoom: 12
@@ -41,3 +71,5 @@ function getDataFromEndpointAsync(callback) {
             console.log(response)
         });
 }
+
+*/
