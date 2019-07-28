61b3ae909f425a9da702f83a1e736a3bf8e516b0
Ubuntu <ubuntu@ip-172-31-93-218.ec2.internal>
2019-07-28 11:01:16 +0000

    1. Added bootstrap cards and color formats. 2. Added some comments to both index.html and js.app files.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 // Variables declarations
-
+// ----------------------------------------------------------------
 // --- Store map instance
 var map;
+
 // --- Store API link
-// CityBikes API 
-var epCityBike = "https://api.citybik.es/v2/networks";
-var epBikeWise = "https://bikewise.org:443/api/v2/incidents";
+var epCityBike = "https://api.citybik.es/v2/networks";          // CityBikes API
+var epBikeWise = "https://bikewise.org:443/api/v2/incidents";   // BikeWise API
 
 
 // Data source of CityBike API
@@ -16,10 +16,6 @@ function getDataFromCityBikeAsync(callback) {
             // console.log(fbCityBike);
             callback(fbCityBike);
             
-            // let results = response.data.features[0].geometry.coordinates;
-            // // trigger the callback
-            // callback(results);
-            // console.log(response)
         });
 }
 
@@ -27,9 +23,12 @@ function getDataFromCityBikeAsync(callback) {
 function getDataFromBikeWiseAsync(callback) {
     axios.get(epBikeWise)
         .then(function(response){
+            
             // let fbBikeWise = response.data.incidents;
             // callback(fbBikeWise);
             // console.log(fbBikeWise);
+            console.log(response);
+            
         });
 }
 
@@ -48,7 +47,7 @@ function initMap() {
     // Render Singapore coordinates on Google Map
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 1.35, lng: 103.81 },
-        zoom: 11
+        zoom: 3
     });
 }
 
